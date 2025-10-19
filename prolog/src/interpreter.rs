a57dc10555bfb71aa2652385e3197f14b8d6a7eb
tsukimizake <shomasd@gmail.com>
2025-10-19 15:05:07 +0900

    updaet machine type

--- prolog/src/interpreter.rs
+++ prolog/src/interpreter.rs
@@ -22,6 +22,7 @@ enum RegStackCell {
 
 #[allow(unused)]
 enum Frame {
+    Base {},
     Environment {
         prev_ep: Rc<Frame>,
         return_pc: Rc<WamInstr>,
@@ -34,22 +35,33 @@ enum Frame {
 }
 
 #[allow(unused)]
-struct Machine {
+struct Machine<'a> {
     heap: Vec<RegStackCell>, // Hレジスタはheap.len()
-    stack: Vec<Frame>,
+    stack: Vec<Rc<Frame>>,
     trail: Vec<Rc<RegStackCell>>,     // 変更された参照セルのヒープ位置
     arg_registers: Vec<RegStackCell>, // TODO runtime_sized_arrayにする可能性
     other_registers: Vec<RegStackCell>,
-    program: Vec<WamInstr>,
-    pc: usize,
-    ep: Rc<Frame>, // 現在の環境フレーム先頭 ()
-    cp: Rc<Frame>, // 現在の選択ポイントフレーム先頭 (B)
-    name_table: HashMap<usize, String>,
+    program: &'a [WamInstr],
+    pc: &'a WamInstr,
+    env_p: Rc<Frame>,    // 現在の環境フレーム先頭
+    choice_p: Rc<Frame>, // 現在の選択ポイントフレーム先頭
 }
 
 #[allow(unused)]
-impl Machine {
-    pub fn lookup_name(&self, id: usize) -> &String {
-        self.name_table.get(&id).unwrap()
+impl<'a> Machine<'a> {
+    fn new(program: &'a [WamInstr]) -> Self {
+        let stack_head = Rc::new(Frame::Base {});
+        let stack = vec![stack_head.clone()];
+        Self {
+            heap: Vec::new(),
+            stack: stack,
+            trail: Vec::new(),
+            arg_registers: Vec::new(),
+            other_registers: Vec::new(),
+            program,
+            pc: &program[0],
+            env_p: stack_head.clone(),
+            choice_p: stack_head,
+        }
     }
 }
