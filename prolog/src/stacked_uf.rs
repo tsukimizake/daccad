88a675bd4dddd3d1d45970b33b8a200ae3265d35
tsukimizake <shomasd@gmail.com>
2025-12-10 15:52:02 +0900

    auto tests

--- prolog/src/stacked_uf.rs
+++ prolog/src/stacked_uf.rs
@@ -97,3 +97,128 @@ fn visit_tail_root<'a, T: Eq + Hash>(tail: &'a [HashMap<Rc<T>, Rc<T>>], x: &Rc<T
     }
     Rc::clone(x)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_find_unconnected() {
+        let mut uf = StackedUf::new();
+        let a = Rc::new(1);
+        let b = Rc::new(2);
+
+        let root_a = uf.find(&a);
+        let root_b = uf.find(&b);
+
+        assert!(Rc::ptr_eq(&root_a, &a));
+        assert!(Rc::ptr_eq(&root_b, &b));
+        assert!(!Rc::ptr_eq(&root_a, &root_b));
+    }
+
+    #[test]
+    fn test_union_and_find() {
+        let mut uf = StackedUf::new();
+        let a = Rc::new(1);
+        let b = Rc::new(2);
+
+        uf.union(&a, &b);
+
+        let root_a = uf.find(&a);
+        let root_b = uf.find(&b);
+
+        assert!(Rc::ptr_eq(&root_a, &root_b));
+    }
+
+    #[test]
+    fn test_transitive_union() {
+        let mut uf = StackedUf::new();
+        let a = Rc::new(1);
+        let b = Rc::new(2);
+        let c = Rc::new(3);
+
+        uf.union(&a, &b);
+        uf.union(&b, &c);
+
+        let root_a = uf.find(&a);
+        let root_c = uf.find(&c);
+
+        assert!(Rc::ptr_eq(&root_a, &root_c));
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_pop_choicepoint_empty_panics() {
+        let mut uf: StackedUf<i32> = StackedUf::new();
+        uf.pop_choicepoint();
+    }
+
+    #[test]
+    fn test_backtrack_undoes_union() {
+        let mut uf = StackedUf::new();
+        let a = Rc::new(1);
+        let b = Rc::new(2);
+
+        uf.union(&a, &b);
+        let root_before = uf.find(&a);
+
+        uf.push_choicepoint();
+        let c = Rc::new(3);
+        uf.union(&a, &c);
+
+        let root_a = uf.find(&a);
+        let root_c = uf.find(&c);
+        assert!(Rc::ptr_eq(&root_a, &root_c));
+
+        uf.pop_choicepoint();
+
+        let root_after = uf.find(&a);
+        let root_c_after = uf.find(&c);
+
+        assert!(Rc::ptr_eq(&root_after, &root_before));
+        assert!(!Rc::ptr_eq(&root_after, &root_c_after));
+    }
+
+    #[test]
+    fn test_choicepoint_isolation() {
+        let mut uf: StackedUf<i32> = StackedUf::new();
+        let a = Rc::new(1);
+        let b = Rc::new(2);
+
+        uf.union(&a, &b);
+
+        uf.push_choicepoint();
+        let c = Rc::new(3);
+        let d = Rc::new(4);
+        uf.union(&c, &d);
+
+        let root_a = uf.find(&a);
+        let root_b = uf.find(&b);
+        let root_c = uf.find(&c);
+        let root_d = uf.find(&d);
+
+        assert!(Rc::ptr_eq(&root_a, &root_b));
+        assert!(Rc::ptr_eq(&root_c, &root_d));
+        assert!(!Rc::ptr_eq(&root_a, &root_c));
+
+        uf.pop_choicepoint();
+
+        let root_a_after = uf.find(&a);
+        let root_b_after = uf.find(&b);
+        assert!(Rc::ptr_eq(&root_a_after, &root_b_after));
+    }
+
+    #[test]
+    fn test_string_values() {
+        let mut uf = StackedUf::new();
+        let x = Rc::new("x".to_string());
+        let y = Rc::new("y".to_string());
+
+        uf.union(&x, &y);
+
+        let root_x = uf.find(&x);
+        let root_y = uf.find(&y);
+
+        assert!(Rc::ptr_eq(&root_x, &root_y));
+    }
+}
